4ea0856dc9a5659802371d08b361ddd3e1ca0e56
Jiang,Jianxing <jianxing.jiang@pactera.com>
2024-07-24 05:24:17 +0800

    Update test-path-7.js

--- test-path-7.js
+++ test-path-7.js
@@ -1,2 +1,2 @@
 const div7 = document.getElementById("test-script-7");
-div7.innerHTML = "https:///..jiangjianxing.github.io/../../test/test-path-7.js loaded";
+div7.innerHTML = "https:///jiangjianxing.github.io/../../test/test-path-7.js loaded";
