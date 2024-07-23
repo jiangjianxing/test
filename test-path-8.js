a1bdd51ff99ed43f400384726af5940cb4c7b46d
Jiang,Jianxing <jianxing.jiang@pactera.com>
2024-07-24 05:24:47 +0800

    Update test-path-8.js

--- test-path-8.js
+++ test-path-8.js
@@ -1,2 +1,2 @@
 const div8 = document.getElementById("test-script-8");
-div8.innerHTML = "https:///..jiangjianxing.github.io/../t/../../test/../test/1/.././test-path-8.js loaded";
+div8.innerHTML = "https:///jiangjianxing.github.io/../t/../../test/../test/1/.././test-path-8.js loaded";
